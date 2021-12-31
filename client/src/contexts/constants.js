991b0f0df7af195c0a6e463863341d40114f3df8
Tuyen <9a5.tuyen@gmail.com>
2021-12-31 15:19:39 +0700

    Add Code

--- client/src/contexts/constants.js
+++ client/src/contexts/constants.js
@@ -1,7 +1,7 @@
 export const apiUrl =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:5000/api"
-    : "sthing";
+    : "https://pacific-mesa-43647.herokuapp.com/api";
 
 export const LOCAL_STORAGE_TOKEN_NAME = "learnit-mearn";
 
